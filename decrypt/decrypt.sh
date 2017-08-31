55c880c903c08361b49d98c9cfb3e09007b1c795
Paul Billing-Ross <pbilling@stanford.edu>
2017-08-31 16:06:07 -0700

    Cleaned up junk code

--- decrypt/decrypt.sh
+++ decrypt/decrypt.sh
@@ -1,18 +1,11 @@
 #!/bin/bash
 
-## OLD
-#mkdir -p /mnt/data/output
-#decrypt_dflow_batch.sh ${TARFILE} ${PASSPHRASE} ${ASCPAIR} ${OUTPUTPATH}
-
-#UNENCRYPTED=$(${TARFILE%.tar.pgp})
 NOEXT=${TARFILE%.tar.pgp}
 UNENCRYPTED=${NOEXT##*/}
 
-# Decrypt
 echo "Importing asc file into gpg"
 gpg --import ${ASCPAIR}
 
-# Decrypt
 echo "Decrypting ${encrypted_file} using gpg"
 gpg \
 --batch \
@@ -21,7 +14,5 @@ gpg \
 --output ${NOEXT}.tar \
 --decrypt ${TARFILE}
 
-# Unpack
 echo "Unpacking tar file unencrypted.tar"
-#tar -xvf /mnt/datadisk/input/unencrypted.tar -C ${OUTPUTPATH}
 tar -xvf ${NOEXT}.tar -C ${OUTPUTPATH}
