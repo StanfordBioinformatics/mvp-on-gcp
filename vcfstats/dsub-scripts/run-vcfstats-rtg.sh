43ac7f8334a1212e697f77a881e12009021bfe40
Paul Billing-Ross <pbilling@stanford.edu>
2017-09-28 16:14:01 -0700

    throwaway

--- vcfstats/dsub-scripts/run-vcfstats-rtg.sh
+++ vcfstats/dsub-scripts/run-vcfstats-rtg.sh
@@ -3,7 +3,7 @@
 dsub \
 --zones ${mvp_region} \
 --project ${mvp_project} \
---logging gs://${mvp_project}/dsub/vcfstats/rtg-tools/logs/${date_stamp} \
+--logging gs://${mvp_bucket}/dsub/vcfstats/rtg-tools/logs/${date_stamp} \
 --image gcr.io/${mvp_project}/rtg-tools:1.0 \
 --command 'rtg vcfstats ${INPUT} > ${OUTPUT}' \
 --tasks ${mvp_hub}/vcfstats/dsub-inputs/rtg-tools/gs-bina-vcfstats-rtg-missing-${date_stamp}.tsv ${dsub_range} \
